27ecd18755a8c55354387e8073e25f3a3efd6d10
Miguel Trubiano <trubiano59@hotmail.com>
2022-12-13 14:37:33 -0300

    arreglo login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //Importar los servicios
 //import { PorfolioService } from './servicios/porfolio.service'; Si lo ponemos asi tambien funciona
 
 //Importar los componentes
 import { AppComponent } from './app.component';
-import { LoginComponent } from './modals/login/login.component';
+//import { LoginComponent } from './modals/login/login.component';
+import { LoginComponent } from './login/login.component';
 import { RedesComponent } from './redes/redes.component';
 import { BannerComponent } from './banner/banner.component';
 import { ExperienceComponent } from './experience/experience.component';
@@ -36,52 +37,57 @@ import { InterestsdashboardComponent } from './interestsdashboard/interestsdashb
 import { NabvarComponent } from './nabvar/nabvar.component';
 import { NabvardashboardComponent } from './nabvardashboard/nabvardashboard.component';
 import { AppRoutingModule } from './app-routing.module';
+//import { LoginComponent_1 as LoginComponent } from "./modals/login/login.component";
 
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RedesComponent,
-    BannerComponent,
-    IndexComponent,
-    ExperienceComponent,
-    EducationComponent,
-    SkillsComponent,
-    InterestsComponent,
-    AwardsComponent,
-    WorkComponent,
-    ProyectosComponent,
-    ErrorComponent,
-    MainComponent,
-    AcercademiComponent,
-    DashboardComponent,
-    BotonlogoutComponent,
-    AcercademidashboardComponent,
-    ExperiencedashboardComponent,
-    EducationdashboardComponent,
-    ProyectosdashboardComponent,
-    SkillsdashboardComponent,
-    WorkdashboardComponent,
-    BannerdashboardComponent,
-    AwardsdashboardComponent,
-    InterestsdashboardComponent,
-    NabvarComponent,
-    NabvardashboardComponent,
-    
-    
-   
-  ],
-  
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-
-  ],
-  //Agregamos el servicio al array de providers
-  providers: [],    //PorfolioService si lo ponemos asi tambien funciona
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RedesComponent,
+        BannerComponent,
+        IndexComponent,
+        ExperienceComponent,
+        EducationComponent,
+        SkillsComponent,
+        InterestsComponent,
+        AwardsComponent,
+        WorkComponent,
+        ProyectosComponent,
+        ErrorComponent,
+        MainComponent,
+        AcercademiComponent,
+        DashboardComponent,
+        BotonlogoutComponent,
+        AcercademidashboardComponent,
+        ExperiencedashboardComponent,
+        EducationdashboardComponent,
+        ProyectosdashboardComponent,
+        SkillsdashboardComponent,
+        WorkdashboardComponent,
+        BannerdashboardComponent,
+        AwardsdashboardComponent,
+        InterestsdashboardComponent,
+        NabvarComponent,
+        NabvardashboardComponent,
+    ],
+    //Agregamos el servicio al array de providers
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        LoginComponent
+    ]
 })
 export class AppModule { }
+
+
+
+
+
+//export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// importamos las librerias de formulario que vamos a necesitar
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  
+  // Inyectar en el constructor el formBuilder
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    //creamos el grupo de control para el formulario login
+    this.form= this.formBuilder.group({
+      password:['',[Validators.required, Validators.minLength(8)]],
+      mail:['',[Validators.required, Validators.email]]
+    })
+    }  
 
-  ngOnInit(): void {
+ 
+  ngOnInit() {}
+  //Metodos para el formulario
+  //toma el dato de la password
+  get Password(){
+    return this.form.get("password");
+  }
+  //toma el dato del mail
+  get Mail(){
+    return this.form.get("mail");
+  }
+  get PasswordValid(){
+    return this.Password?.touched && !this.Password?.valid;
+  }
+  get MailValid(){
+    return this.Password?.touched && !this.Password?.valid;
+  }
+  onEnviar(event: Event){
+    //Detenemos la propagación o ejecución del comportamiento submit de un form
+    event.preventDefault;
+
+    if (this.form.valid){
+      alert("todo salio bien: !Enviar formulario!")
+      //llamamos nuestro servicio para enviar los datos al servidor
+    }else{
+      this.form.markAllAsTouched();
+      alert("todo salio mal: !No Enviar formulario!")
+    } 
   }
 
 }
